5356d2d4925f3c6a3431aafc3bd2a3c958b6dae9
MohammedZuhairAhmed <mohammed.ahmed@contentstack.com>
2024-05-22 15:22:49 +0530

    Feat: Integration with TSGEN library

--- src/commands/tsgen.ts
+++ src/commands/tsgen.ts
@@ -1,11 +1,28 @@
-import {Command} from '@contentstack/cli-command'
-import {FlagInput, flags} from '@contentstack/cli-utilities'
-import {getGlobalFields, stackConnect, StackConnectionConfig, generateGraphQLTypeDef} from '../lib/stack/client'
-import {ContentType} from '../lib/stack/schema'
-import tsgenRunner from '../lib/tsgen/runner'
+import { Command } from "@contentstack/cli-command";
+import { FlagInput, flags } from "@contentstack/cli-utilities";
+import * as path from "path";
+import * as fs from "fs";
+import { generateTS, graphqlTS } from "tsgen-lib";
+
+export type StackConnectionConfig = {
+  apiKey: string;
+  token: string;
+  region: any;
+  environment: string;
+  branch?: string;
+};
+
+function createOutputPath(outputFile: string) {
+  const outputPath = path.resolve(process.cwd(), outputFile);
+  const dirName = path.dirname(outputPath);
+
+  fs.mkdirSync(dirName, { recursive: true });
+
+  return outputPath;
+}
 
 export default class TypeScriptCodeGeneratorCommand extends Command {
-  static description = 'generate TypeScript typings from a Stack';
+  static description = "generate TypeScript typings from a Stack";
 
   static examples = [
     '$ csdx tsgen -a "delivery token alias" -o "contentstack/generated.d.ts"',
@@ -16,117 +33,150 @@ export default class TypeScriptCodeGeneratorCommand extends Command {
   ];
 
   static flags: FlagInput = {
-    'token-alias': flags.string({
-      char: 'a',
-      description: 'delivery token alias',
+    "token-alias": flags.string({
+      char: "a",
+      description: "delivery token alias",
       hidden: false,
       multiple: false,
       required: true,
     }),
 
     output: flags.string({
-      char: 'o',
-      description: 'full path to output',
+      char: "o",
+      description: "full path to output",
       hidden: false,
       multiple: false,
       required: true,
     }),
 
     prefix: flags.string({
-      char: 'p',
+      char: "p",
       description: 'interface prefix, e.g. "I"',
       hidden: false,
       multiple: false,
-      default: '',
+      default: "",
       required: false,
     }),
 
     doc: flags.boolean({
-      char: 'd',
-      description: 'include documentation comments',
+      char: "d",
+      description: "include documentation comments",
       default: true,
       allowNo: true,
     }),
 
     branch: flags.string({
-      description: 'branch',
+      description: "branch",
       hidden: false,
       multiple: false,
     }),
 
-    'include-system-fields': flags.boolean({
-      description: 'include system fields in generated types',
+    "include-system-fields": flags.boolean({
+      description: "include system fields in generated types",
       default: false,
     }),
 
-    'api-type': flags.string({
-      default: 'rest',
+    "api-type": flags.string({
+      default: "rest",
       multiple: false,
-      options: ['rest', 'graphql'],
-      description: '[Optional] Please enter an API type to generate the type definitions.',
+      options: ["rest", "graphql"],
+      description:
+        "[Optional] Please enter an API type to generate the type definitions.",
     }),
 
     namespace: flags.string({
-      description: '[Optional]Please enter a namespace for the GraphQL API type to organize the generated types.',
+      description:
+        "[Optional]Please enter a namespace for the GraphQL API type to organize the generated types.",
     }),
   };
 
   async run() {
     try {
-      const {flags} = await this.parse(TypeScriptCodeGeneratorCommand)
+      const { flags } = await this.parse(TypeScriptCodeGeneratorCommand);
 
-      const token = this.getToken(flags['token-alias'])
-      const prefix = flags.prefix
-      const includeDocumentation = flags.doc
-      const outputPath = flags.output
-      const branch = flags.branch
-      const includeSystemFields = flags['include-system-fields']
-      const namespace = flags.namespace
+      const token = this.getToken(flags["token-alias"]);
+      const prefix = flags.prefix;
+      const includeDocumentation = flags.doc;
+      const filePath = flags.output;
+      const branch = flags.branch;
+      const includeSystemFields = flags["include-system-fields"];
+      const namespace = flags.namespace;
 
-      if (token.type !== 'delivery') {
-        this.warn('Possibly using a management token. You may not be able to connect to your Stack. Please use a delivery token.')
+      const outputPath = createOutputPath(filePath);
+
+      if (token.type !== "delivery") {
+        this.warn(
+          "Possibly using a management token. You may not be able to connect to your Stack. Please use a delivery token."
+        );
       }
 
       if (!outputPath || !outputPath.trim()) {
-        this.error('Please provide an output path.', {exit: 2})
+        this.error("Please provide an output path.", { exit: 2 });
+      }
+      let region;
+      switch (this.region.name) {
+        case "NA":
+          region = "US";
+          break;
+        case "AZURE-NA":
+          region = "AZURE_NA";
+          break;
+        case "AZURE-EU":
+          region = "AZURE_EU";
+          break;
+        case "GCP-NA":
+          region = "GCP_NA";
+          break;
+        default:
+          region = this.region.name.toUpperCase();
       }
 
       const config: StackConnectionConfig = {
         apiKey: token.apiKey,
         token: token.token,
-        region: (this.region.name === 'NA') ? 'us' : this.region.name.toLowerCase(),
-        environment: token.environment || '',
-        branch: branch || null,
-      }
+        region: region,
+        environment: token.environment || "",
+        branch: branch || undefined,
+      };
 
-      if (flags['api-type'] === 'graphql') {
-        const result = await generateGraphQLTypeDef(config, outputPath, namespace)
-        if (result) {
-          this.log(`Successfully added the GraphQL schema type definitions to '${result.outputPath}'.`)
-        } else {
-          this.log('No schema found in the stack! Please use a valid stack.')
+      if (flags["api-type"] === "graphql") {
+        try {
+          const result = await graphqlTS({
+            token: config.token,
+            apiKey: config.apiKey,
+            region: config.region,
+            environment: config.environment,
+            branch: config.branch,
+            namespace: namespace,
+          });
+          fs.writeFileSync(outputPath, result);
+          this.log(
+            `Successfully added the GraphQL schema type definitions to '${outputPath}'.`
+          );
+        } catch (error: any) {
+          this.log(error.error_message);
         }
       } else {
-        const [client, globalFields] = await Promise.all([stackConnect(this.deliveryAPIClient.Stack, config, this.cdaHost), getGlobalFields(config, this.cdaHost)])
-
-        let schemas: ContentType[] = []
-        if (client.types?.length) {
-          if ((globalFields as any)?.global_fields?.length) {
-            schemas = schemas.concat((globalFields as any).global_fields as ContentType)
-            schemas = schemas.map(schema => ({
-              ...schema,
-              schema_type: 'global_field',
-            }))
-          }
-          schemas = schemas.concat(client.types)
-          const result = await tsgenRunner(outputPath, schemas, prefix, includeDocumentation, includeSystemFields)
-          this.log(`Wrote ${result.definitions} Content Types to '${result.outputPath}'.`)
-        } else {
-          this.log('No Content Types exist in the Stack.')
+        try {
+          const result = await generateTS({
+            token: config.token,
+            apiKey: config.apiKey,
+            region: config.region,
+            environment: config.environment,
+            branch: config.branch,
+            tokenType: "delivery",
+            includeDocumentation: includeDocumentation,
+            prefix,
+            systemFields: includeSystemFields,
+          });
+          fs.writeFileSync(outputPath, result);
+          this.log(`Wrote typeDef of Content Types to '${outputPath}'.`);
+        } catch (error: any) {
+          this.log(error.error_message);
         }
       }
     } catch (error: any) {
-      this.error(error as any, {exit: 1})
+      this.error(error as any, { exit: 1 });
     }
   }
 }
